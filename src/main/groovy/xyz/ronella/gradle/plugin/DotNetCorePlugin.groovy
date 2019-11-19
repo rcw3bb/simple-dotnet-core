150382957220421e8ab37e4d53e99a5651ea554e
ron <ron@ronella.xyz>
2019-11-19 13:09:07 +1300

    Add group to tasks.

--- src/main/groovy/xyz/ronella/gradle/plugin/DotNetCorePlugin.groovy
+++ src/main/groovy/xyz/ronella/gradle/plugin/DotNetCorePlugin.groovy
@@ -22,30 +22,43 @@ class DotNetCorePlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
         project.task('dotnetBuild', type: DotNetBuildTask,
+                group: 'Simple .NET Core',
                 description: 'Build a .Net project.')
         project.task('dotnetClean', type: DotNetCleanTask,
+                group: 'Simple .NET Core',
                 description: 'Clean build outputs of a .NET project.')
         project.task('dotnetInfo', type: DotNetInfoTask,
+                group: 'Simple .NET Core',
                 description: 'Display .NET Core information.')
         project.task('dotnetMsBuild', type: DotNetMsBuildTask,
+                group: 'Simple .NET Core',
                 description: 'Run Microsoft Build Engine (MSBuild) command.')
         project.task('dotnetNewConsole', type: DotNetNewConsoleTask,
+                group: 'Simple .NET Core',
                 description: 'Create a new .NET Console project.')
         project.task('dotnetPack', type: DotNetPackTask,
+                group: 'Simple .NET Core',
                 description: 'Create a NuGet package.')
         project.task('dotnetPublish', type: DotNetPublishTask,
+                group: 'Simple .NET Core',
                 description: 'Publish a .NET project for deployment.')
         project.task('dotnetRestore', type: DotNetRestoreTask,
+                group: 'Simple .NET Core',
                 description: 'Restore dependencies specified in a .NET project.')
         project.task('dotnetTask', type: DotNetTask,
+                group: 'Simple .NET Core',
                 description: 'Execute any available dotnet core commands.')
         project.task('dotnetTest', type: DotNetTestTask,
+                group: 'Simple .NET Core',
                 description: 'Run unit tests using the test runner specified in a .NET project.')
         project.task('dotnetVersion', type: DotNetVersionTask,
+                group: 'Simple .NET Core',
                 description: 'Display .NET Core SDK version is use.')
         project.task('dotnetRun', type: DotNetRunTask,
+                group: 'Simple .NET Core',
                 description: 'Build and run a .NET project output.')
         project.task('dotnetCreateGlobalJson', type: DotNetCreateGlobalJsonTask,
-                description: 'Generates a global.json file.')
+                group: 'Simple .NET Core',
+                description: 'Generate a global.json file.')
     }
 }
